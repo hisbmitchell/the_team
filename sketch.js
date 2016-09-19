f239eb9f2110e64e13d5869b0458d62bda2826d6
hisbmitchell <heathermitchell.ottawa@gmail.com>
2016-09-19 13:37:59 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -81,6 +81,10 @@ function mousePressed() {
   if (isOverImage == true) {
     println("CLICK");
     currentImage = int(random(38));
+    randNum = int(random(0,10));
+    if randNum == (1){
+      text("NOT THE TEAM!!" 0, 500);
+    }
     //currentImage++;
     //img = theTeam[1];
   }
